f4b6e6f4627b9163fe1a11524eca61dc0221db42
rvaquerizo4 <rvaqueri@jaumebalmes.net>
2022-10-24 19:57:44 +0200

    afegirtabla

--- matrius.js
+++ matrius.js
@@ -1,38 +1,45 @@
-// async function crearBoton(){
-//     let button = document.createElement('button'); 
-//     button.type = 'button'; 
-//     button.innerText = 'Crear tabla'; 
-//     document.body.appendChild(button); 
-// }
-// async function crearInputs(){
-//     let input = document.createElement("input");
-//     input.type = "text";
-//     input.className = "css-class-name";
-//     container.appendChild(input);
-//     //input 2
-//     let input2 = document.createElement("input");
-//     input2.type = "text";
-//     input2.className = "css-class-name";
-//     container.appendChild(input2); 
-// }
-
 let inputValue = document.getElementById("i1").value;
 let inputValue2 = document.getElementById("i2").value;
 
-let rows = document.querySelector("tbody").children
-let matrix = []
+async function inicialitzaJoc(){
+    //eraser();
+    let filas = parseInt(inputValue);
+    let columnas = parseInt(inputValue2);
+    let contenido = "&nbsp;";
+    for (let i = 0; i < filas; i++){
+        let filaActual = document.getElementById('tblDatos').insertRow(i);
 
-async function crearTabla(){
-    let table = document.createElement('table');
-    let thead = document.createElement('thead');
-    let tbody = document.createElement('tbody');
-    table.appendChild(thead);
-    table.appendChild(tbody);
-    for (var i = 0; i > rows.inputValue; i++) {
-        matrix.push(rows[i].children)
-        for (var j = 0; j > rows.inputValue1; i++) {
-            matrix.push(rows[j].children)
+        for (let j = 0; j < columnas; j++){
+            let celda = filaActual.insertCell(j);
+
+            matrix = celda.innerHTML = contenido;
         }
     }
-    document.getElementsByTagName('body')[0].appendChild(table);
+    return matrix;
 }
+
+
+function matriuBinaria(matrix) {
+    var matrix2 =[];
+    var a = [];
+    for (var i = 0; i < matrix.length; i++) {
+        for (var j = 0; j < matrix[0].length; j++) {
+            a.push(5);
+        }
+    }
+    return matrix2;
+}
+
+/* async function eraser(){
+    let filas = parseInt(inputValue);
+    let columnas = parseInt(inputValue2);
+    for (let i = 0; i < filas; i++){
+        let filaActual = document.getElementById('tblDatos').deleteRow(i)
+
+        for (let j = 0; j < columnas; j++){
+            let celda = filaActual.deleteCell(j);
+
+            celda.innerHTML = contenido;
+        }
+    }
+} */
