420d3a96ad1414e5ddb92a2065eda810675bf099
rvaquerizo4 <rvaqueri@jaumebalmes.net>
2022-11-09 17:59:06 +0100

    buscaminas 2 y medio

--- matrius.js
+++ matrius.js
@@ -38,18 +38,49 @@ function matriuBinaria(matrix) {
     return matrix2;
 }
 
-function paintCheckerboard() {
-    let filas = document.querySelector("tbody").children
-    let matrix = []
-    for (var i = 0; i < filas.length; i++) {
-        matrix.push(filas[i].children)
+//aÑadir input para el numero de mina con el id MinesIntroduides
+function inicialitzaMines(MinesIntroduides, i1, i2) {
+    let matriu = [];
+    let minas = MinesIntroduides;
+    for (let i = 0; i < i2; i++) {
+        let filas = [];
+        for (let j = 0; j < i1; j++) {
+            filas.push(0)
+        }
+        matriu.push(filas);
     }
-    for (let i=0; i < matrix.length; i++) {
-        for (let j=0; j < matrix[0].length; j++) {
-            if((j%2) == 0 && (i%2) == 0)
-            matrix[i][j].style.backgroundColor = "red";
-            else if((j%2) != 0 && (i%2) != 0)
-            matrix[i][j].style.backgroundColor = "red";
+
+    while (minas > 0) {
+        let aleatorio1 = Math.trunc(Math.random() * (i1));
+        let aleatorio2 = Math.trunc(Math.random() * (i2));
+        if (matriu[aleatorio1][aleatorio2] != 1) {
+            matriu[aleatorio1][aleatorio2] = 1;
+            minas--;
         }
     }
+    return matriu;
+}
+
+let minas2;
+function Imines(minas2) {
+    let taula = document.getElementsByTagName("tbody")[0];
+    for (let i = 0; i < minas2.length; i++) {
+        for (let j = 0; j < minas2[0].length; j++) {
+            if (minas2[i][j] == 1) {
+                taula.children[i].children[j].style.backgroundColor = "red";
+            }
+        }
+    }
+}
+
+
+function iniJoc() {
+    inicialitzaJoc();
+    let MinesIntroduides = document.getElementById("MinesIntroduides").valueAsNumber;
+    let i1 = document.getElementById("i1").valueAsNumber;
+    let i2 = document.getElementById("i2").valueAsNumber;
+
+    minas2 = inicialitzaMines(MinesIntroduides, i1, i2);
+    Imines(minas2);
+
 }
