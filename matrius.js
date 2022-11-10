7e7d31ab5647ea850d20738c1b4195a48a8ed54d
Raúl Vaquerizo <113448166+rvaquerizo4@users.noreply.github.com>
2022-11-10 22:53:12 +0100

    buscaminas 3

--- matrius.js
+++ matrius.js
@@ -3,13 +3,9 @@ let filas = document.querySelector("tbody").children
 for (var i = 0; i < rows.length; i++) {
     matrix.push(rows[i].children)
 }
+let minas;
 
 async function inicialitzaJoc(){
-    let matrix = [];
-    let filas = document.querySelector("tbody").children
-    for (var i = 0; i < rows.length; i++) {
-        matrix.push(rows[i].children)
-    }
     let filas = document.getElementById("i1").valueAsNumber;
     let columnas = document.getElementById("i1").valueAsNumber;
     let contenido = "&nbsp;";
@@ -46,7 +42,7 @@ function matriuBinaria(matrix) {
 //aÑadir input para el numero de mina con el id MinesIntroduides
 function inicialitzaMines(MinesIntroduides, i1, i2) {
     let matriu = [];
-    let minas = MinesIntroduides;
+    let m = MinesIntroduides;
     for (let i = 0; i < i2; i++) {
         let filas = [];
         for (let j = 0; j < i1; j++) {
@@ -55,39 +51,57 @@ function inicialitzaMines(MinesIntroduides, i1, i2) {
         matriu.push(filas);
     }
 
-    while (minas > 0) {
+    while (m > 0) {
         let aleatorio1 = Math.trunc(Math.random() * (i1));
         let aleatorio2 = Math.trunc(Math.random() * (i2));
         if (matriu[aleatorio1][aleatorio2] != 1) {
             matriu[aleatorio1][aleatorio2] = 1;
-            minas--;
+            m--;
         }
     }
     return matriu;
 }
 
-let minas;
+
 function rellenar(minas) {
-    let tablamatriu = document.getElementsByTagName("tbody")[0];
-    for (let i = 0; a < mines.length; i++) {
-        for (let j = 0; j < mines[0].length; j++) {
-            if (mines[i][j] == 1) {
-                tablamatriu.children[i].children[j].style.backgroundColor = "red";
+    let tabla = document.getElementsByTagName("tbody")[0];
+    for (let i = 0; i < minas.length; i++) {
+        for (let j = 0; j < minas[0].length; j++) {
+            if (minas[i][j] == 1) {
+                
+                tabla.children[i].children[j].style.backgroundColor = "red";
 
             }
         }
     }
 }
 
-function comienzaJoc() {
+function iniJoc() {
     inicialitzaJoc();
     let MinesIntroduides = document.getElementById("MinesIntroduides").valueAsNumber;
     let i1 = document.getElementById("i1").valueAsNumber;
     let i2 = document.getElementById("i2").valueAsNumber;
 
-    minas = inicialitzaMines(MinesIntroduides, i1, i2);
+    let minas = inicialitzaMines(MinesIntroduides, i1, i2);
     rellenar(minas);
+    premCelda();
 }
+
+function premCelda() {
+    var aux = document.getElementById("tabla");
+    var celdaClic = coord => {
+        if (coord.target.style.backgroundColor == "red") {
+            window.alert(`Has clicado en las coordenadas ${coord.target.id} lamentablemente es una mina`);
+        }
+        else {
+            window.alert(`Has clicado en las coordenadas ${coord.target.id} i te has salvado, no es una mina`);
+        }
+    }
+    aux.addEventListener("click", celdaClic);
+}
+
+
+
 
 
 
